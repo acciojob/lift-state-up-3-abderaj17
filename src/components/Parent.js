ff9d99260a7c468293bf381cafcf52152254e8e6
Adarsh Agrahari <agrahariadarsh68@gmail.com>
2025-03-22 15:37:07 +0530

    cpm

--- src/components/Parent.js
+++ src/components/Parent.js
@@ -1,20 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Parent = () => {
+    const [selectedOption, setSelectedOption] = useState();
+
+    const handleClick = (e)=>{
+        setSelectedOption(e.target.value);
+    }
   return (
     <div>
         <div className='parent'>
             <h1>Parent Component</h1>
             <div className='child1'>
                 <h2>Child Component 1</h2>
-                <button>Option 1</button>
+                <button value="Option 1" onClick={handleClick}>Option 1</button>
             </div>
             <div className='child2'>
                 <h2>Child Component 2</h2>
-                <button>Option 2</button>
+                <button value="Option 2" onClick={handleClick}>Option 2</button>
             </div>
             <div className='selectedOption'>
-                Selected Option: 
+                Selected Option: {selectedOption}
             </div>
         </div>
     </div>
