d74c2068bec5e8111835ea5281c078ff269e0189
Adarsh Agrahari <agrahariadarsh68@gmail.com>
2025-03-22 15:26:59 +0530

    cl

--- src/components/Parent.js
+++ src/components/Parent.js
@@ -3,7 +3,20 @@ import React from 'react'
 const Parent = () => {
   return (
     <div>
-        <div></div>
+        <div className='parent'>
+            <h1>Parent Component</h1>
+            <div className='child1'>
+                <h2>Child Component 1</h2>
+                <button>Option 1</button>
+            </div>
+            <div className='child2'>
+                <h2>Child Component 2</h2>
+                <button>Option 2</button>
+            </div>
+            <div className='selectedOption'>
+                Selected Option: 
+            </div>
+        </div>
     </div>
   )
 }
